6c2a1aef100b44e44e4718fa42742cbe1716ddd1
Chandler <chanalexberger@gmail.com>
2019-09-29 12:03:32 -0400

    added basic event listeners to controller module

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
     
-    //private
-    var x = 23;
-    //private
-    var add = function(a) {
-        return x + a;
-    }
-    //public
-    //works because of closures - has access to x and add variables
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    // Some Code
     
 })();
 
-
+// UI CONTROLLER
 var UIController = (function() {
+    
     // Some code
 
 })();
 
-
+// GLOBAL APP CONTROLLER
+// controls what happens based on events
 var controller = (function(budgetCtrl, UICtrl) {
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+
+    var ctrlAddItem = function () {
+        // 1. Get the field input data
+
+        // 2. Add item to the budget controller
+
+        // 3. Add item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display budget on the UI
+
+        console.log('It works');
     }
+
+    //Add button event listener
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    // Enter key press event listener
+    document.addEventListener('keypress', function(event) {
+        // if Enter key was pressed
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController, UIController);
