425de648a9333f5f4b3ffbdcbefe5fb144c42f3b
Chandler <chanalexberger@gmail.com>
2019-09-29 11:42:01 -0400

    added UIController and controller modules

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 var budgetController = (function() {
     
+    //private
     var x = 23;
-    
+    //private
     var add = function(a) {
         return x + a;
     }
-    
+    //public
+    //works because of closures - has access to x and add variables
     return {
         publicTest: function(b) {
-            console.log(add(b));
+            return add(b);
         }
     }
     
 })();
+
+
+var UIController = (function() {
+    // Some code
+
+})();
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+    var z = budgetCtrl.publicTest(5);
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+})(budgetController, UIController);
