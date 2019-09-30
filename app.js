6691ce038517f4cb64d5a2a73052f80830c636eb
Chandler <chanalexberger@gmail.com>
2019-09-30 18:19:11 -0400

    added event listener for deleting income/expense and ctrlDeleteItem

--- app.js
+++ app.js
@@ -115,7 +115,8 @@ var UIController = (function() {
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     };
     
     return {
@@ -134,11 +135,11 @@ var UIController = (function() {
             // Create HTML string with placeholder text
             if(type === 'inc') {
                 element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
             if(type === 'exp') {
                 element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
             
             // Replace the placeholder text with actual data
@@ -210,6 +211,9 @@ var controller = (function(budgetCtrl, UICtrl) {
                 ctrlAddItem();
             }
         });
+
+        // Event listener for deleting income/expense - using event delegation
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
     };
 
     var ctrlAddItem = function () {
@@ -237,6 +241,22 @@ var controller = (function(budgetCtrl, UICtrl) {
             updateBudget();
         }
     };
+
+    var ctrlDeleteItem = function(event) {
+        var itemID, splitID, type, ID;
+
+        // Gets the income/expense id for the item to delete
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        // Only move forward if itemID <> ""
+        if (itemID){
+            // itemID format = "inc-x" or "exp-x"
+            // split inc/exp and number (x)
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = splitID[1];
+        }
+    }
 
     var updateBudget = function() {
         // 1. Calculate the budget
